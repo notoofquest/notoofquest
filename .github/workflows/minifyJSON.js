cbbee7cc3b25a4586d774dca57a405f85e6c3af9
williamd5 <66572326+williamd5@users.noreply.github.com>
2021-05-22 15:20:31 +0300

    Update minifyJSON.js

--- .github/workflows/minifyJSON.js
+++ .github/workflows/minifyJSON.js
@@ -1,13 +1,10 @@
 "use strict";
-const fs = require("fs");
-const path = require("path");
-
-
+const fs = require("fs"),
+      path = require("path");
 fs.readFile(path.join(__dirname, "../../config.json"), function (err, data) {
-	if (err) throw new Error(err);
+  if (err) throw new Error(err);
   fs.writeFileSync(path.join(__dirname, "../../config.min.json"), JSON.stringify(JSON.parse(data.toString())), function (err) {
      if (err) throw new Error(err);
   });
 });
-
 console.log("Done!");
